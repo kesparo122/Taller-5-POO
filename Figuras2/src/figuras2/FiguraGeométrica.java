4e98c3c9ff4064eec5a31e32226e74aaef8530ab
Santiago <55521570+SantiagoLondono8@users.noreply.github.com>
2022-11-19 23:35:16 -0500

    Add files via upload

---
+++ Figuras2/src/figuras2/FiguraGeométrica.java
@@ -0,0 +1,20 @@
+package figuras2;
+
+public class FiguraGeométrica {
+    private double volumen;
+    private double superficie;
+    
+    public void setVolumen(double volumen) {
+        this.volumen = volumen;
+}
+    public void setSuperficie(double superficie) {
+        this.superficie = superficie;
+}
+    public double getVolumen() {
+        return this.volumen;
+}
+
+    public double getSuperficie() {
+        return this.superficie;
+}
+}
---
+++ Figuras2/src/figuras2/Figuras2.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package figuras2;
+
+/**
+ *
+ * @author ACER
+ */
+public class Figuras2 {
+
+public static void main(String[] args) {
+    VentanaPrincipal miVentanaPrincipal;
+    miVentanaPrincipal= new VentanaPrincipal(); 
+    miVentanaPrincipal.setVisible(true);
+    miVentanaPrincipal.setResizable(false);
+    }
+}
